6bf84610e270aff047b7c9b1f3cc1aca92541778
Rhys Smoker <r.smoker@gmail.com>
2022-10-19 19:02:16 -0700

    buttons all appear at this point, but they are attached to the body, I'd like them to be attached to buttonContainer instead

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
-// buttonContainer will have event listener
+// hooks into DOM
 var buttonContainer = document.querySelector(".buttonContainer");
 var welcomeButton = document.querySelector("#welcomeButton");
 var welcomeDiv = document.querySelector("#Welcome");
 var question_1Prompt = document.querySelector("#question_1");
 
-var Questions = [
-	{
-		Q: "What is Alaska's Capital City?",
-		Choices: ["Anchorage", "Fairbanks", "Olympia", "Juneau"],
-		TA: "Juneau",
-	},
-	{
-		Q: "How many roads must a man walk down, before you call him a man?",
-		Choices: ["Forty-two", "Sixteen", "Seventy-Seven", "Blowing in the Wind"],
-		TA: "Blowing in the Wind",
-	},
-	{
-		Q: "How will the world end?",
-		Choices: ["Fire", "Ice", "A fizzle", "A bang"],
-		TA: "Fire",
-	},
-	{
-		Q: "Why did the chicken cross the road?",
-		Choices: [
-			"Better View",
-			"Sour Grapes",
-			"To race the Hare",
-			"Greener Grass",
-		],
-		TA: "Sour Grapes",
-	},
-];
+//Lets making the buttons with javascript
+var buttonsPer = 4; //The number of buttons per question
+var quantityOfQuestions = 4; //The number of questions in this quiz
+//Quiz Maker. This will make a quiz' worth of questions with buttons
+for (let I = 0; I < quantityOfQuestions; I++) {
+	//Questions Entry Section
+	var Questions = [
+		{
+			Q: "What is Alaska's Capital City?",
+			Choices: ["Anchorage", "Fairbanks", "Olympia", "Juneau"],
+			TA: "Juneau",
+		},
+		{
+			Q: "How many roads must a man walk down, before you call him a man?",
+			Choices: ["Forty-two", "Sixteen", "Seventy-Seven", "Blowing in the Wind"],
+			TA: "Blowing in the Wind",
+		},
+		{
+			Q: "How will the world end?",
+			Choices: ["Fire", "Ice", "A fizzle", "A bang"],
+			TA: "Fire",
+		},
+		{
+			Q: "Why did the chicken cross the road?",
+			Choices: [
+				"Better View",
+				"Sour Grapes",
+				"To race the Hare",
+				"Greener Grass",
+			],
+			TA: "Sour Grapes",
+		},
+	];
+	//Single quizquestion maker. This section will repeatedly make a div with 4 buttons
+	var buttonDiv = document.createElement("div"); //makes a question div
+	document.body.appendChild(buttonDiv); //I want to append this to "buttonContainer"
+	buttonDiv.innerHTML = Questions[I].Q; // prints the relevant quiz question
+	for (let i = 0; i < buttonsPer; i++) {
+		//puts multiple choice answers on buttons
+		var ghostButton = document.createElement("button"); //makes a button
+		ghostButton.innerHTML = Questions[I].Choices[i]; //puts a choice answer on
+		buttonDiv.appendChild(ghostButton); //appends it to div above , which is appended to the body
+	}
+}
 
-console.log(question_1Prompt);
-console.log(Questions[0].Q);
-question_1Prompt.textContent = Questions[0].Q;
-Questions[0].Choices;
-console.log(Questions[0].Choices);
-
-//loop through the choices array
-//for each , createElement , google how to create a button element
-//for each element created, append to the DOM in buttonContainer
-
-// welcomeButton.addEventListener("click", function () {
-//reveal the hidden buttons
-// buttonContainer.classList.remove("hidden");
-// welcomeDiv.classList.add("hidden");
-//hide the welcome div
-// });
+welcomeButton.addEventListener("click", function () {
+	//reveal the hidden buttons
+	buttonContainer.classList.remove("hidden");
+	welcomeDiv.classList.add("hidden");
+	//hide the welcome div
+});
 
 // buttonContainer.addEventListener("click", function (event) {
 // 	var clickedOn = event.target;
